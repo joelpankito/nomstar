5315a36ec2dd4e81aa0b5af9b96604fe8eefb098
Joel Pankito <joelpankito@yahoo.com>
2019-09-02 17:36:26 +0000

    Add a good message describing what you actually changed since your last commit in here

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
- before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+ before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
 def index
   @places = Place.paginate(:page => params[:page], :per_page => 3)
@@ -10,8 +10,12 @@ def new
 end
   
 def create
-    current_user.places.create(place_params)
+  @place = current_user.places.create(place_params)
+  if @place.valid?
     redirect_to root_path
+  else
+    render :new, status: :unprocessable_entity
+  end
 end
 
 def show
@@ -32,13 +36,22 @@ def update
   if @place.user != current_user
     return render plain: 'Not Allowed', status: :forbidden
   end
-  
+
   @place.update_attributes(place_params)
-  redirect_to root_path
+  if @place.valid?
+    redirect_to root_path
+  else
+    render :edit, status: :unprocessable_entity
+  end
 end
 
 def destroy
   @place = Place.find(params[:id])
+
+  if @place.user != current_user
+    return render plain: 'Not Allowed', status: :forbidden
+  end
+
   @place.destroy
   redirect_to root_path
 end
